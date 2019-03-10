bef18e3d073f2243aff06b674b06852d9ce24e22
xQoNam <kandeckigs1234@interia.pl>
2019-03-10 14:54:02 +0100

    Few fixes

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -9,10 +9,12 @@ public class GameController : MonoBehaviour
     private Player player;
     public TextMeshProUGUI scoreText;
     public TextMeshProUGUI loseScoreText;
+    public TextMeshProUGUI highScoreText;
     // Start is called before the first frame update
     void Start()
     {
         player = FindObjectOfType<Player>();
+        highScoreText.text = "HIGH SCORE:" + PlayerPrefs.GetInt("HighScore", 0).ToString();
     }
     private void Update()
     {
@@ -22,6 +24,11 @@ public class GameController : MonoBehaviour
     public void SetScore()
     {
         loseScoreText.text= "YOUR SCORE:  " + score.ToString();
+        if(score > PlayerPrefs.GetInt("HighScore",0))
+        {
+            PlayerPrefs.SetInt("HighScore", score);
+            highScoreText.text ="HIGH SCORE: " + score.ToString();
+        }
         if(player.HP!=0)
         {
             scoreText.text = "Score:  " + score.ToString();
--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -2,9 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class MainMenu : MonoBehaviour
 {
+    public TextMeshProUGUI highScore;
+    private void Start()
+    {
+        highScore.text = "HIGH SCORE: " + PlayerPrefs.GetInt("HighScore", 0).ToString();
+    }
     public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
